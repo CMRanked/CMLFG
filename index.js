1c21a0a0f75afa3c02018188bfdd67249a781c72
CMRanked <154623190+CMRanked@users.noreply.github.com>
2024-01-10 10:33:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
+// app.js
+
 const express = require('express');
-const fs = require('fs');
-const bodyParser = require('body-parser');
 const app = express();
+const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// Middleware to parse JSON requests
+app.use(express.json());
 
+// Sample route
 app.get('/', (req, res) => {
-    fs.readFile('lfg.txt', 'utf8', (err, data) => {
-        if (err) throw err;
-        res.send(`
-            <form action="/submit" method="post">
-                <input type="text" name="username" placeholder="Username" required>
-                <input type="text" name="region" placeholder="Region" required>
-                <input type="text" name="otherData" placeholder="Other Data" required>
-                <button type="submit">Submit</button>
-            </form>
-            <pre>${data}</pre>
-        `);
-    });
+  res.send('Hello, this is your Node.js API!');
 });
 
-app.post('/submit', (req, res) => {
-    const { username, region, otherData } = req.body;
-    const data = `Username: ${username}, Region: ${region}, Other Data: ${otherData}\n`;
-    fs.appendFile('lfg.txt', data, (err) => {
-        if (err) throw err;
-        res.redirect('/');
-    });
+// Sample POST route to handle JSON data
+app.post('/api/data', (req, res) => {
+  const data = req.body;
+  res.json({ message: 'Data received successfully', data });
 });
 
-app.listen(3000, () => console.log('Server started on port 3000'));
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
